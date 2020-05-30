90f5b70125d50b47f7fb000a7997cf20f85bf8fe
sevskii <sevskii111@gmail.com>
2020-05-30 16:45:05 +0300

    keyboard test

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-game:
-	g++ main.cpp -o play -I include -L lib -l SDL2-2.0.0 -l SDL2_image-2.0.0
+OS := $(shell uname)
+
+OBJS = main.cpp
+
+CC = g++
+
+COMPILER_FLAGS = 
+
+ifeq ($(OS), Darwin) #MacOs
+	LINKER_FLAGS = -I include -L lib -l SDL2-2.0.0 -l SDL2_image-2.0.0
+else
+	LINKER_FLAGS = -lSDL2 -lSDL2_image
+endif
+
+OBJ_NAME = play
+
+all : $(OBJS)
+	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
--- main.cpp
+++ main.cpp
@@ -1,41 +1,23 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
 #include <cmath>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-//Starts up SDL and creates window
 bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
 void close();
 
-//Loads individual image as texture
 SDL_Texture *loadTexture(std::string path);
-
-//The window we'll be rendering to
 SDL_Window *gWindow = NULL;
-
-//The window renderer
 SDL_Renderer *gRenderer = NULL;
 
 bool init()
 {
-    //Initialization flag
     bool success = true;
 
-    //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
@@ -43,14 +25,12 @@ bool init()
     }
     else
     {
-        //Set texture filtering to linear
         if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
         {
             printf("Warning: Linear texture filtering not enabled!");
         }
 
-        //Create window
-        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        gWindow = SDL_CreateWindow("Червяки", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (gWindow == NULL)
         {
             printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
@@ -58,7 +38,6 @@ bool init()
         }
         else
         {
-            //Create renderer for window
             gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
             if (gRenderer == NULL)
             {
@@ -67,10 +46,8 @@ bool init()
             }
             else
             {
-                //Initialize renderer color
                 SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-                //Initialize PNG loading
                 int imgFlags = IMG_INIT_PNG;
                 if (!(IMG_Init(imgFlags) & imgFlags))
                 {
@@ -81,15 +58,6 @@ bool init()
         }
     }
 
-    return success;
-}
-
-bool loadMedia()
-{
-    //Loading success flag
-    bool success = true;
-
-    //Nothing to load
     return success;
 }
 
@@ -119,83 +87,101 @@ SDL_Texture *loadTexture(std::string path)
     }
     else
     {
-        //Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
         if (newTexture == NULL)
         {
             printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
         }
 
-        //Get rid of old loaded surface
         SDL_FreeSurface(loadedSurface);
     }
 
     return newTexture;
 }
 
+enum KeyPressSurfaces
+{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+SDL_Surface *gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+
 int main(int argc, char *args[])
 {
-    //Start up SDL and create window
     if (!init())
     {
         printf("Failed to initialize!\n");
     }
     else
     {
-        //Load media
-        if (!loadMedia())
-        {
-            printf("Failed to load media!\n");
-        }
-        else
-        {
-            //Main loop flag
-            bool quit = false;
 
-            //Event handler
-            SDL_Event e;
+        //Main loop flag
+        bool quit = false;
 
-            //While application is running
-            while (!quit)
+        //Event handler
+        SDL_Event e;
+
+        //While application is running
+        while (!quit)
+        {
+            //Handle events on queue
+            while (SDL_PollEvent(&e) != 0)
             {
-                //Handle events on queue
-                while (SDL_PollEvent(&e) != 0)
+                //User requests quit
+                if (e.type == SDL_QUIT)
                 {
-                    //User requests quit
-                    if (e.type == SDL_QUIT)
+                    quit = true;
+                }
+                else if (e.type == SDL_KEYDOWN)
+                {
+                    SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+
+                    //Select surfaces based on key press
+                    switch (e.key.keysym.sym)
                     {
+                    case SDLK_q:
                         quit = true;
+                        break;
+                    case SDLK_SPACE:
+                        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+                        SDL_RenderFillRect(gRenderer, &fillRect);
+                    default:
+                        break;
                     }
                 }
-
-                //Clear screen
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-                SDL_RenderClear(gRenderer);
-
-                //Render red filled quad
-                SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-                SDL_RenderFillRect(gRenderer, &fillRect);
-
-                //Render green outlined quad
-                SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
-                SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
-                SDL_RenderDrawRect(gRenderer, &outlineRect);
-
-                //Draw blue horizontal line
-                SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-                SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
-
-                //Draw vertical line of yellow dots
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
-                for (int i = 0; i < SCREEN_HEIGHT; i += 4)
-                {
-                    SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
-                }
-
-                //Update screen
-                SDL_RenderPresent(gRenderer);
             }
+
+            //Clear screen
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(gRenderer);
+
+            //Render red filled quad
+            SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+            SDL_RenderFillRect(gRenderer, &fillRect);
+
+            //Render green outlined quad
+            SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
+            SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+            SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+            //Draw blue horizontal line
+            SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+            SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+            //Draw vertical line of yellow dots
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+            for (int i = 0; i < SCREEN_HEIGHT; i += 4)
+            {
+                SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+            }
+
+            //Update screen
+            SDL_RenderPresent(gRenderer);
         }
     }
 
